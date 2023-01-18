9459ca38cda890a00846aefcee0861cb16532765
JonathanSeife <112643740+JonathanSeife@users.noreply.github.com>
2023-01-18 15:25:08 -0500

    added alerts

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
 export class NumberOfEvents extends Component {
   state = {
     numEvents: 32,
+    errorText: "",
   };
 
   handleInputChanged = (event) => {
     const value = event.target.value;
-    this.setState({ numEvents: value });
+    if (value < 1 || value > 32) {
+      this.setState({
+        numberOfEvents: value,
+        errorText: "Enter number from 1 to 32",
+      });
+    } else {
+      this.setState({
+        numberOfEvents: event.target.value,
+        errorText: "",
+      });
+    }
     this.props.updateNumberOfEvents(value);
   };
 
@@ -28,6 +40,9 @@ export class NumberOfEvents extends Component {
             onChange={this.handleInputChanged}
           />
         </label>
+        <div>
+          <ErrorAlert text={this.state.errorText} />
+        </div>
       </div>
     );
   }
